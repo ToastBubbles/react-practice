b17448d80ec19319666e6f9ef9fa1275da3be759
Jeffrey Neal <jeffneal11@gmail.com>
2022-12-07 09:02:11 -0600

    change Clock to countdown timer

--- react-practice/src/Components/Clock.js
+++ react-practice/src/Components/Clock.js
@@ -3,13 +3,36 @@ import React from "react";
 class Clock extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { date: new Date() };
+    this.state = { date: new Date(), timer: 5 };
   }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    if (this.state.timer <= 0) {
+      this.setState({
+        timer: "boom",
+      });
+    } else if (this.state.timer > 0) {
+      this.setState({
+        date: new Date(),
+        timer: (this.state.timer -= 1),
+      });
+    }
+  }
+
   render() {
     return (
       <div>
         <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        <h2>{this.state.timer.toString()}.</h2>
+        {/* <h2>It is {this.state.date.toLocaleTimeString()}.</h2> */}
       </div>
     );
   }
